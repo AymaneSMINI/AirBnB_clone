21bf1478e95d4fff2f52c77fa8a9645db96f91aa
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2023-02-14 18:07:30 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,51 @@
 #!/usr/bin/python3
-
+"""Defines the BaseModel class."""
+import models
 from uuid import uuid4
 from datetime import datetime
-import storage
 
 
 class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
 
-    """This is the BaseModel Class"""
     def __init__(self, *args, **kwargs):
-        if kwargs != {}:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        value = datetime.fromisoformat(value)
-                    self.__dict__[key] = value
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
-            storage.new(self)
+        """Initialize a new BaseModel.
 
-    def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
+        """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        dictionary = {'__class__': self.__class__.__name__}
-        for key, value in self.__dict__.items():
-            if key == 'updated_at' or key == 'created_at':
-                value = value.isoformat()
-            dictionary[key] = value
-        return dictionary
+        """Return the dictionary of the BaseModel instance.
 
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
 
-def main():
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    print(my_model.to_dict())
-    my_model_json = my_model.to_dict()
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-    print('---------------------------------------------------------------')
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-    print('-----------------------------------------------')
-    print(my_model is my_new_model)
-
-
-if __name__ == '__main__':
-    main()
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
